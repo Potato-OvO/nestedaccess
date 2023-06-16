eea3d0bf90da00980bd7f4036da6cd1fa995bc8c
Potato-OvO <1224141267@qq.com>
2023-06-16 12:08:05 +0800

    setup file update

--- setup.py
+++ setup.py
@@ -1,20 +1,130 @@
-from setuptools import find_packages, setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'nestedaccess'
+DESCRIPTION = 'Nestedaccess is a simple, elegant way to retrieve nested data from deep dictionaries or lists.'
+URL = 'https://github.com/Potato-OvO/nestedaccess'
+EMAIL = 'wkl1224141267@gmail.com'
+AUTHOR = 'Potato-OvO'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = '0.1.1'
+LICENSE = 'MIT'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        readme = '\n' + f.read()
+except FileNotFoundError:
+    readme = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
 
 
-with open('README.md') as f:
-    long_description = f.read()
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
 
 
+# Where the magic happens:
 setup(
-    name="nestedaccess",
-    packages=find_packages(),
-    version="0.1.1",
-    description="Get nested data of dictionary or list.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Potato-OvO",
-    author_email="wkl1224141267@gmail.com",
-    license="MIT",
-    url="https://github.com/Potato-OvO/nestedaccess",
-    python_requires=">=3.6"
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    py_modules=[NAME],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license=LICENSE,
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        f'License :: OSI Approved :: {LICENSE} License',
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
