413408f886df2ccd0bb043218a381102117d4404
Potato-OvO <1224141267@qq.com>
2023-06-16 10:04:50 +0800

    setup file update

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import find_packages, setup
 
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
 setup(
     name="nestedaccess",
-    packages=find_packages(include=["nestedaccess"]),
-    version="0.1.0",
-    description="Get nested data of dictionary or list",
-    author="potato",
+    packages=find_packages(),
+    version="0.1.1",
+    description="Get nested data of dictionary or list.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Potato-OvO",
     author_email="wkl1224141267@gmail.com",
     license="MIT",
     url="https://github.com/Potato-OvO/nestedaccess",
